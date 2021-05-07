d74f3e202e17a6cfc1dd8f4e954088b604957497
gabriel kukiel <gabrielkukiel@gmail.com>
2021-05-07 01:05:22 -0400

    [NEW] Lint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 0,
+    'no-shadow': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    'object-curly-spacing': ['error', 'always'],
+  },
 };
